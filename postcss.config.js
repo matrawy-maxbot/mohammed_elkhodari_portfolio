faa17a1acceaa29399e308d751baa1a2f88312fd
Mohammed Elkhodary <114000998+matrawy-maxbot@users.noreply.github.com>
2025-12-24 23:58:39 +0200

    v1.1.15

--- postcss.config.js
+++ postcss.config.js
@@ -2,13 +2,5 @@ export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-    cssnano: {
-      preset: ['default', {
-        discardComments: {
-          removeAll: true,
-        },
-        normalizeWhitespace: true,
-      }]
-    }
   },
 }
